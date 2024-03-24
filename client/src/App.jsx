7e9bdb8967bdffe4d9dc2372d61a5dd1dfd18eb8
balankdharan <balankdharan@gmail.com>
2024-03-24 20:26:48 +0530

    register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import Login from "./pages/login/Login";
 import "./App.css";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <div>
-      <Login />
+      <Register />
     </div>
   );
 }
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,33 @@
 import "./register.scss";
 
 const Register = () => {
-  return <div className="register">register</div>;
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea eaque
+            veritatis repellendus, laborum deserunt repellat exercitationem
+            illo. Nesciunt qui modi eum itaque totam! Adipisci cumque illo porro
+            aut odit officiis.
+          </p>
+          <span>Do you have an account? </span>
+          <button>Login</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form action="">
+            <input type="text" name="" id="" placeholder="User Name" />
+            <input type="text" name="" id="" placeholder="Name" />
+            <input type="email" name="" id="" placeholder="Email" />
+            <input type="password" name="" id="" placeholder="Password" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
