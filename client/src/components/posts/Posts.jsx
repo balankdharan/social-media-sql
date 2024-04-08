87dca45334fbaae1f5992426e2b8afe270bf6e96
balankdharan <balankdharan@gmail.com>
2024-04-08 21:03:11 +0530

    Posts and stories

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,7 +1,34 @@
+import Post from "../post/Post";
 import "./posts.scss";
 
 const Posts = () => {
-  return <div className="posts">Posts</div>;
+  const posts = [
+    {
+      id: "1",
+      name: "Test man",
+      userId: 1,
+      profilePic:
+        "https://www.thecompassncsd.com/wp-content/uploads/2024/02/child-phone-Social-Media-1387329287-967x544-1.jpg",
+      desc: "lorem ipsum dolor sit amet, consectetur adipiscing",
+      img: "https://graphicsfamily.com/wp-content/uploads/edd/2021/07/Free-Editable-Food-Instagram-Story-Design-1180x664.jpg",
+    },
+    {
+      id: "2",
+      name: "Test man",
+      userId: 1,
+      profilePic:
+        "https://www.thecompassncsd.com/wp-content/uploads/2024/02/child-phone-Social-Media-1387329287-967x544-1.jpg",
+      desc: "lorem ipsum dolor sit amet, consectetur adipiscing",
+      img: "https://elements-cover-images-0.imgix.net/6f1931c7-7147-425a-814d-b76263f567cf?auto=compress%2Cformat&fit=max&w=900&s=3673bd225fcbd2d9e03fda00fbb1566a",
+    },
+  ];
+  return (
+    <div className="posts">
+      {posts.map((post) => (
+        <Post post={post} key={post.id} />
+      ))}
+    </div>
+  );
 };
 
 export default Posts;
--- client/src/components/stories/Stories.jsx
+++ client/src/components/stories/Stories.jsx
@@ -1,7 +1,46 @@
+import { useContext } from "react";
 import "./stories.scss";
+import { AuthContext } from "../../context/authContext";
 
 const Stories = () => {
-  return <div className="stories">Stories</div>;
+  const { currentUser } = useContext(AuthContext);
+  const stories = [
+    {
+      id: 1,
+      name: "Test man",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvmntayKq1xRQ2h06lToEYqZGjBJLevK63Cc_DpBgpmQ&s",
+    },
+    {
+      id: 2,
+      name: "Test man",
+      img: "https://i.pinimg.com/originals/74/c0/6d/74c06dd03468d7a3aba5a0ee63bc3553.jpg",
+    },
+    {
+      id: 3,
+      name: "Test man",
+      img: "https://img.freepik.com/free-vector/food-instagram-stories-template-collection_52683-11440.jpg",
+    },
+    {
+      id: 4,
+      name: "Test man",
+      img: "https://i.pinimg.com/originals/74/c0/6d/74c06dd03468d7a3aba5a0ee63bc3553.jpg",
+    },
+  ];
+  return (
+    <div className="stories">
+      <div className="story">
+        <img src={currentUser.profilePic} alt="storyImage" />
+        <span>{currentUser.name}</span>
+        <button>+</button>
+      </div>
+      {stories.map((story) => (
+        <div className="story" key={story.id}>
+          <img src={story.img} alt="storyImage" />
+          <span>{story.name}</span>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Stories;
