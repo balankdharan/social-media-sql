3925381bd36c8633d9f1a71c56f36f006a50a115
balankdharan <balankdharan@gmail.com>
2024-04-10 17:26:09 +0530

    Profiel

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,7 +1,71 @@
 import "./profile.scss";
-
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Posts from "../../components/posts/Posts";
 const Profile = () => {
-  return <div className="profile">profile</div>;
+  return (
+    <div className="profile">
+      <div className="images">
+        <img
+          src="https://elements-video-cover-images-0.imgix.net/files/366410468/Food+Instagram+Story+Pack.jpg?auto=compress%2Cformat&h=506&w=900&fit=min&s=e4437752d83220f0d12345853257a9e1"
+          alt="cover"
+          className="cover"
+        />
+        <img
+          src="https://marketplace.canva.com/EAFpm06B7Bs/2/0/900w/canva-yellow-black-white-organic-illustrative-sustainable-habits-food-waste-instagram-story-QsyUEzmLius.jpg"
+          alt="profile"
+          className="profile"
+        />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="http://facebook.com">
+              <FacebookTwoToneIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <TwitterIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <LinkedInIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <PinterestIcon fontSize="large" />
+            </a>
+          </div>
+          <div className="center">
+            <span>Test name</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>USA</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>google.com</span>
+              </div>
+            </div>
+            <button>Follow</button>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+        <Posts />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
