60d6cfacd9622e5264374c0ae61ef530c476b68e
balankdharan <balankdharan@gmail.com>
2024-04-21 20:45:35 +0530

    UI

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
 import "./register.scss";
+import { useState } from "react";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({});
   return (
     <div className="register">
       <div className="card">
