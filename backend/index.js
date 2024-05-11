0fbc0307cc7cccf408707b72647bdef930a1eb92
balankdharan <balankdharan@gmail.com>
2024-05-11 19:46:55 +0530

    Share and image Upload using multer , upload folder in frontend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import multer from "multer";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -16,6 +17,21 @@ app.use(cors({ origin: true, credentials: true }));
 
 app.use(cookieParser());
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
