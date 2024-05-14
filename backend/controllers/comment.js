5ba5aa533cb8c7e8473677f7a2873193facf1035
balankdharan <balankdharan@gmail.com>
2024-05-14 19:44:48 +0530

    comments API and integration

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -1,5 +1,6 @@
 import moment from "moment";
 import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
 
 export const getComments = (req, res) => {
   const commentQuery = `SELECT c.*, u.id AS userId, name, profilePic FROM comments AS c JOIN users AS u ON (u.id = c.userId) WHERE c.postId = ? ORDER BY c.createdAt DESC`;
@@ -7,5 +8,28 @@ export const getComments = (req, res) => {
   db.query(commentQuery, [req.query.postId], (err, data) => {
     if (err) return res.status(500).json({ err });
     return res.status(200).json(data);
+  });
+};
+
+export const addComment = (req, res) => {
+  const token = req.cookies.accessToken;
+
+  if (!token) return res.status(401).json("Not logged in ! ");
+
+  jwt.verify(token, "secretKey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const commentQuery =
+      "INSERT INTO comments (`desc`,`createdAt`,`userId`,`postId`) VALUES (?)";
+    const values = [
+      req.body.desc,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      userInfo.id,
+      req.body.postId,
+    ];
+    db.query(commentQuery, [values], (err, data) => {
+      if (err) return res.status(500).json({ err });
+      return res.status(200).json("Comment has created");
+    });
   });
 };
--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -1,19 +1,36 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./comments.scss";
 import { AuthContext } from "../../context/authContext";
-import { useQuery } from "@tanstack/react-query";
 import { makeRequest } from "../../axios";
 import moment from "moment";
-
+import { useMutation, useQueryClient, useQuery } from "@tanstack/react-query";
 const Comments = ({ postId }) => {
   const { currentUser } = useContext(AuthContext);
+  const [desc, setDesc] = useState("");
 
   const { isLoading, error, data } = useQuery({
     queryKey: ["comments"],
     queryFn: () =>
       makeRequest.get("/comments?postId=" + postId).then((res) => res.data),
   });
-  console.log(data);
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (newComment) => {
+      return makeRequest.post("/comments", newComment);
+    },
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries(["comments"]);
+    },
+  });
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    mutation.mutate({ desc, postId: postId });
+    setDesc("");
+  };
   // const comments = [
   //   {
   //     id: 1,
@@ -36,14 +53,19 @@ const Comments = ({ postId }) => {
     <div className="comments">
       <div className="write">
         <img src={currentUser.profilePic} alt="imgofpic" />
-        <input type="text" placeholder="Write a comment" />
-        <button>Send</button>
+        <input
+          type="text"
+          placeholder="Write a comment"
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)}
+        />
+        <button onClick={handleClick}>Send</button>
       </div>
       {isLoading
         ? "Loading"
         : data.map((comment) => (
             <div className="comment">
-              <img src={comment.profilePicture} alt="imgofpic" />
+              <img src={comment.profilePic} alt="imgofpic" />
               <div className="info">
                 <span>{comment.name}</span>
                 <p>{comment.desc}</p>
