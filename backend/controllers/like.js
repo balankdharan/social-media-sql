7490e875c4c4c6f925c9ab719a8ea6ff8003e365
balankdharan <balankdharan@gmail.com>
2024-05-16 23:10:17 +0530

    likes with mutation

---
+++ backend/controllers/like.js
@@ -0,0 +1,46 @@
+import { db } from "../connect.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const getLikes = (req, res) => {
+  const commentQuery = "SELECT userId FROM likes WHERE postId = ?";
+
+  db.query(commentQuery, [req.query.postId], (err, data) => {
+    if (err) return res.status(500).json({ err });
+    return res.status(200).json(data.map((like) => like.userId));
+  });
+};
+
+export const deleteLike = (req, res) => {
+  const token = req.cookies.accessToken;
+
+  if (!token) return res.status(401).json("Not logged in ! ");
+
+  jwt.verify(token, "secretKey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const deleteQuery = "DELETE FROM likes WHERE `userId` = ? AND `postId`= ? ";
+
+    db.query(deleteQuery, [userInfo.id, req.query.postId], (err, data) => {
+      if (err) return res.status(500).json({ err });
+      return res.status(200).json("like has been deleted");
+    });
+  });
+};
+
+export const addLike = (req, res) => {
+  const token = req.cookies.accessToken;
+
+  if (!token) return res.status(401).json("Not logged in ! ");
+
+  jwt.verify(token, "secretKey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const likeQuery = "INSERT INTO likes (`userId`,`postId`) VALUES (?)";
+    const values = [userInfo.id, req.body.postId];
+    db.query(likeQuery, [values], (err, data) => {
+      if (err) return res.status(500).json({ err });
+      return res.status(200).json("Likes has created");
+    });
+  });
+};
--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -6,12 +6,42 @@ import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
 import Comments from "../comments/Comments";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import moment from "moment";
+import { useMutation, useQueryClient, useQuery } from "@tanstack/react-query";
+import { makeRequest } from "../../axios";
+import { AuthContext } from "../../context/authContext";
 
 const Post = ({ post }) => {
   const [commentOpen, setCommentOpen] = useState(false);
   const liked = false;
+
+  const { currentUser } = useContext(AuthContext);
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["likes", post.id],
+    queryFn: () =>
+      makeRequest.get("/likes?postId=" + post.id).then((res) => res.data),
+  });
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (liked) => {
+      if (liked) return makeRequest.delete("/likes?postId=" + post.id);
+      return makeRequest.post("/likes", { postId: post.id });
+    },
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries(["likes"]);
+    },
+  });
+
+  // console.log("date", data);
+
+  const handleLike = () => {
+    mutation.mutate(data.includes(currentUser.id));
+  };
   return (
     <div className="post">
       <div className="container">
@@ -36,8 +66,17 @@ const Post = ({ post }) => {
         </div>
         <div className="info">
           <div className="item">
-            {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
-            12 Likes
+            {isLoading ? (
+              "Loading "
+            ) : data.includes(currentUser.id) ? (
+              <FavoriteOutlinedIcon
+                style={{ color: "red" }}
+                onClick={handleLike}
+              />
+            ) : (
+              <FavoriteBorderOutlinedIcon onClick={handleLike} />
+            )}
+            {data?.length} Likes
           </div>
           <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
             <TextsmsOutlinedIcon />5 comments
